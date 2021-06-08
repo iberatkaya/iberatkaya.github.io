2e0749a529d1735c5559f36cb05e47245bb72e0f
iberatkaya <beratkaya1998@gmail.com>
2021-06-08 14:11:54 +0300

    Minor updates

--- src/experiences/experiences.ts
+++ src/experiences/experiences.ts
@@ -23,12 +23,12 @@ export const experiences: IExperience[] = [
       'Developed a Turkish receipt scanner mobile application with React Native named i-expense available at the <a href="https://play.google.com/store/apps/details?id=com.nacsoft.iexpense" target="_blank" rel="noopener noreferrer">Play Store</a> and <a href="https://apps.apple.com/app/i-expense/id1488919379" target="_blank" rel="noopener noreferrer">App Store</a>. Learned about REST and SOAP API\'s, MySQL, and OCR Technology. Used Google Cloud Vision to scan receipts and developed an algorithm to parse the receipts. Uploaded the parsed receipts to Nacsoft\'s system.',
     image: NacsoftImg,
     location: "Kocaeli, Turkey",
-    subtitle: "Nacsoft Yazılım",
+    subtitle: "Nacsoft",
     companyLink: "https://www.nacsoft.com.tr/",
     renderAsHTML: true,
   },
   {
-    title: "Software Engineer",
+    title: "Software Developer",
     date: "Jun 2020 - Nov 2020, Part Time",
     description:
       '<p>Developed Mobile and Web Applications with Flutter and React. Designed and implemented the Back-End and Database of the applications with Firebase and Node.js. Used Cloud Firestore, Cloud Storage, Cloud Functions, Analytics, and Authentication services of Firebase to develop the apps. Wrote Cloud Functions with Node.js and TypeScript.</p> <p>Developed a Mobile and <a href="http://app.unipubs.com/" target="_blank" rel="noopener noreferrer">Web App</a> named UniPubs available on the Play Store (<a href="https://play.google.com/store/apps/details?id=com.customer.unipubs" target="_blank" rel="noopener noreferrer">Student App</a>) and App Store (<a href="https://apps.apple.com/us/app/unipubs-%C3%B6%C4%9Frenci/id1536918993" target="_blank" rel="noopener noreferrer">Student App</a>): <ul> <li>UniPubs is an app used in Turkey where students or stationery stores can upload study notes as PDF\'s or images.</li> <li>The uploaded study notes are then sold to users as an open market.</li> <li>UniPubs has an app for users who want to purchase notes and a separete merchant app for users that want to upload study notes and earn money. The merchant app was released but later removed.</li> <li>The main target audience is university students. Users can filter notes based on universities and faculties.</li><li>For the payment system, we used a Turkish payment company named <a href="https://www.iyzico.com/" target="_blank" rel="noopener noreferrer">iyzico</a>.</li></ul></p>',
